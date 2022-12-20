33eddec4e81e9bd7baf4b7a85b3116f0688abfb1
Rafael Jáber <rafael.jaber@hotmail.com>
2022-12-19 22:05:04 -0300

    Criado o teste para o ClienteRepositorio e realizada migration pra propular o banco.

---
+++ Alura.ByteBank.Infraestrutura.Teste/ClienteRepositorioTestes.cs
@@ -0,0 +1,28 @@
+﻿using Alura.ByteBank.Dados.Repositorio;
+using Alura.ByteBank.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.ByteBank.Infraestrutura.Teste
+{
+    public class ClienteRepositorioTestes
+    {
+        [Fact]
+        public void TestaObterTodosClientes()
+        {
+            // Arrange
+            var _repositorio = new ClienteRepositorio();
+
+            // Act
+            List<Cliente> lista = _repositorio.ObterTodos();
+
+            // Assert
+            Assert.NotNull(lista);
+            Assert.NotEqual(0,lista.Count);
+        }
+    }
+}
